8bd524ef7970c2b680c38ae06f1ebc9116468c33
adlmidl <mrjaxsoni@yandex.ru>
2025-10-17 17:00:04 +0400

    0005 - Добавить таблицу и хранимые процедуры для сущности роли сотрудников

---
+++ migrations/01_create_init_roles.up.sql
@@ -0,0 +1,65 @@
+-- СОЗДАТЬ ТАБЛИЦУ РОЛЕЙ
+CREATE TABLE dbo.svRoles
+(
+    id          INT          DEFAULT 0  NOT NULL PRIMARY KEY, -- id - id роли.
+    name        VARCHAR(150) DEFAULT '' NOT NULL,             -- name - название роли.
+    description VARCHAR(250) DEFAULT '' NOT NULL,             -- description - описание роли.
+);
+
+CREATE INDEX idx_svRoles_id ON dbo.svRoles (id);
+
+CREATE PROCEDURE dbo.svGetRoles -- Процедура получения всех ролей.
+AS
+BEGIN
+    SELECT id, name, description FROM dbo.svRoles;
+END
+GO;
+
+CREATE PROCEDURE dbo.svGetRoleById -- Процедура получения роли по id.
+    @id INT -- id роли.
+AS
+BEGIN
+    SELECT id, name, description FROM dbo.svRoles WHERE id = @id;
+END
+GO;
+
+CREATE PROCEDURE dbo.svGetRoleByName -- Процедура получения роли по имени.
+    @name VARCHAR(150) -- имя роли.
+AS
+BEGIN
+    SELECT id, name, description FROM dbo.svRoles WHERE name = @name;
+END
+GO;
+
+CREATE PROCEDURE dbo.svGetRoleDeleteById -- Процедура удаления роли по id.
+    @id INT -- id роли.
+AS
+BEGIN
+    DELETE FROM dbo.svRoles WHERE id = @id;
+    exec svUpdRolePerformer;
+END
+GO;
+
+CREATE PROCEDURE dbo.svUpdRolePerformer -- Процедура обновления роли сотрудника.
+AS
+BEGIN
+    UPDATE dbo.svPerformers
+    SET id_role = 0
+    WHERE id_role NOT IN (SELECT id FROM dbo.svRoles); -- id_role = 0, если роли нет в таблице svRoles.
+END
+GO;
+
+INSERT INTO dbo.svRoles
+VALUES (0, 'user', N'Пользователь доступ к просмотру данных');
+INSERT INTO dbo.svRoles
+VALUES (1, 'storekeeper', N'Кладовщик доступ к CRUD операциям с продукцией');
+INSERT INTO dbo.svRoles
+VALUES (2, 'supervisor', N'Руководитель доступ к CRUD операциям с продукцией и редактированию данных');
+INSERT INTO dbo.svRoles
+VALUES (3, 'administrator', N'Администратор доступ ко всем операциям');
+
+-- exec dbo.svGetRoles;
+-- exec dbo.svGetRoleById 1;
+-- exec dbo.svGetRoleByName 'administrator';
+-- exec dbo.svGetRoleDeleteById 1;
+-- exec dbo.svUpdRolePerformer;
